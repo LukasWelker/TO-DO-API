22093287d0a610a36c94d481a4afb25c8df0828b
Welker, Lukas <lukas.welker@tecalliance.de>
2022-10-17 14:16:24 +0200

    Tutorial for API

---
+++ TO-DO-API/TO-DO-API/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TO_DO_API.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        //durch ein Fragezeichen wird der Standardwert eines Rückgabetyps auf null anstatt 0 gesetzt
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+   
+   
+}
---
+++ TO-DO-API/TO-DO-API/Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TO_DO_API.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
